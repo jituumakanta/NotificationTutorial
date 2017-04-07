26dfc57cb52e3c3dce9bbc4aaf7a39e839199451
jituumakanta <jitu.umakanta@gmail.com>
2017-04-07 13:29:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/volmopc1/notificationtutorial/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.volmopc1.notificationtutorial;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNotification();
+            }
+        });
+    }
+
+    private void addNotification() {
+        NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.abc)
+                        .setContentTitle("Notifications Example")
+                        .setContentText("This is a test notification");
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
+}
---
+++ app/src/main/java/com/example/volmopc1/notificationtutorial/NotificationView.java
@@ -0,0 +1,16 @@
+package com.example.volmopc1.notificationtutorial;
+
+/**
+ * Created by volmopc1 on 07-04-2017.
+ */
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NotificationView extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
